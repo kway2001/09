fbae52d8463c21fc7944ba13ab5fd1f3d503e67e
kway2001 <70741593+kway2001@users.noreply.github.com>
2020-10-29 16:22:58 +0900

    practice 5

--- main.c
+++ main.c
@@ -3,22 +3,44 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+{
+	int i, j;
+	
+	for (i=0; i<ROWS; i++)
+		for (j=0; j<COLS; j++)
+			C[i][j] = A[i][j] + B[i][j];
+}
+
+void printMatrix(int A[][COLS])
+{
+	int i, j;
+	
+	for(i=0; i<ROWS; i++) {
+		for (j=0; j<COLS; j++)
+			printf("%d ", A[i][j]);
+		printf("\n");
+	}
+}
+
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int A[ROWS][COLS] = {
+	{2, 3, 0},
+	{8, 9, 1},
+	{7, 0, 5}	};
+	int B[ROWS][COLS] = {
+	{1, 0, 0},
+	{0, 1, 0},
+	{0, 0, 1}	};
+	int C[ROWS][COLS];
 	
-	for (i=0; i<SIZE; i++)
-		grade[i] = rand() % 100 +1;
-		
-	for (i=0; i<SIZE; i++)
-		score[i] = grade[i];
-		
-	for (i=0; i<SIZE; i++)
-		printf("score[%d] = %d (%i)\n", i, score[i], grade[i]);
+	addMatrix(A, B, C);
+	printMatrix(C);
 	
 	return 0;
 }
